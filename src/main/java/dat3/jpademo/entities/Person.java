23777a3c50f4187845dac9c447951e97e49b5d5a
Plebbz <jens.ohlendorff@gmail.com>
2020-09-23 15:51:19 +0200

    first commit

---
+++ src/main/java/dat3/jpademo/entities/Person.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dat3.jpademo.entities;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author jenso
+ */
+@Entity
+public class Person implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long p_id;
+    private String name;
+    private int year;
+
+    public Person() {
+    }
+
+    Person(String name, int year) {
+        this.year = year;
+        this.name = name;
+    }
+
+    public Long getP_id() {
+        return p_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+    
+    
+}
